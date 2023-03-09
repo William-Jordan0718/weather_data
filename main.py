77786a4f4fe6a95813965d572b17298ed687e493
williamjordan <will.jordan0788@gmail.com>
2023-03-09 11:41:24 -0800

    finished creating a new csv from an existing csv

--- main.py
+++ main.py
@@ -10,5 +10,15 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data)
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squireels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squireels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squireels_count, black_squireels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
